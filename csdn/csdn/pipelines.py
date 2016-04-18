c80ebadd5d17280c4dcd793d46bf6cd29c39aa68
zeus <zeus@163.com>
2016-04-18 01:48:17 -0700

    2016.4.18

---
+++ csdn/csdn/pipelines.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import codecs
+import json
+from scrapy import signals
+
+class CsdnPipeline(object):
+    def __init__(self):
+	self.file=codecs.open('data.json','w',encoding='utf-8')
+
+    def process_item(self, item, spider):
+	line=json.dumps(dict(item))+'\n'
+	self.file.write(line.decode("unicode_escape"))
+        return item
+
+    def spider_closed(self,spider):
+	self.file.close()
